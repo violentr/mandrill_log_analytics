8f015b5b544aa88865d92bf11959e6d1bbb0125b
Deniss <den.b@itservicesgroup.co.uk>
2017-09-17 10:42:32 +0100

    Add Percentage of clicked and opened per emailtype
    Calculate percentage based on email type
    
    example: All opened emails devide by uniq
    opened emails by email type, result was
    rounded by 3 number.

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,7 +10,12 @@ class Address < ActiveRecord::Base
     %w(opened clicked).each do |event|
       uniq_email_types.each do |type|
         self.class.send(:define_method, "#{event}_#{type}".to_sym) do
-          self.send(event.to_sym).where(emailtype: type)
+          addresses_by_emailtype = self.send(event.to_sym).where(emailtype: type)
+          addresses_by_event = self.send(event.to_sym)
+          event_rate_by_emailtype =
+            (Float(addresses_by_event.size) / Float(addresses_by_emailtype.uniq.size)).round(3)
+          percents = 100
+          Float(percents/event_rate_by_emailtype).round(3)
         end
       end
     end
